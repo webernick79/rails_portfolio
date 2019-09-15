fc73691c19346fd3973f4c9643d42efe6a45e452
Nick Weber <webernick@icloud.com>
2019-09-15 14:27:57 -0700

    Adds portfolio new/create

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,2 +1,34 @@
 class PortfoliosController < ApplicationController
+  
+  def index
+    @portfolio_items = Portfolio.all
+  end
+
+  def new
+    @portfolio_item = Portfolio.new
+  end
+
+  def create
+    @portfolio_item = Portfolio.new(portfolio_params)
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'Portfolio item successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  # def set_blog
+  #   @blog = Blog.find(params[:id])
+  # end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def portfolio_params
+    params.require(:portfolio).permit(:title, :subtitle, :body)
+  end
+
 end
